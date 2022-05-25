db933526694baa2496f14535e85cec8f9e7b9eb3
Germanilu <lucianogermani93@gmail.com>
2022-05-25 23:41:24 +0200

    Funcion para cambiar de pantalla, añadidas secciones en html y añadido bg img a estilo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+//Cambio de pantalla
+const changeScreen = (numScreen) => {
+    let fasewant = "section" + numScreen; // Concateno la palabra section con el numero de pantalla
+    let arrFases = ["section1", "section2","section3"]; //Creo el array con las 3 pantallas
+    arrFases = arrFases.filter(val => !fasewant.includes(val)); //con .filter elimino del array la pantalla donde estoy y me quedaran solo las otras
+    document.getElementById(fasewant).style.display = "block"; // Enseño la pantalla elegida con display block
+    for (let _f of arrFases) {
+        document.getElementById(_f).style.display = "none"; //Recorro el array y les doy a todas las pantallas (menos la pantalla donde estoy ) un display none
+    }
+}
