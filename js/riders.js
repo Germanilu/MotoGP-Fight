c1ba121550f6b79decad452ca774a744d06cda7a
Germanilu <lucianogermani93@gmail.com>
2022-05-26 13:59:46 +0200

    Creada funcion random e inicio creacion clase riders

---
+++ js/riders.js
@@ -0,0 +1,29 @@
+
+
+//Funcion Random que genera 1 numero de entre 2 que se pasan por parametro.
+const random = (a,b) => Math.floor(Math.random()*(b-a)+(a));
+//Bucle para generar los numeros que quiere el prompt
+for(i =0; i< 2 ; i++){
+    // console.log(random(numeroInferior,numeroSuperior));
+}
+
+
+
+
+
+
+//Creo la clase Riders 
+// class Riders {
+//     constructor(name,hp,att,def,luck){
+//         this.name = name;
+//         this.hp = hp;
+//         this.att = att;
+//         this.def = def;
+//         this.luck = luck;
+//     }
+
+//     attack(){
+        
+//     }
+// }
+
